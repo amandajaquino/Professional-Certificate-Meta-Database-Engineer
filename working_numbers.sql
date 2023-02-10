77833979bdb9d16e60bb376796f75d503fb223e7
Amanda <jord_amanda@yahoo.com.br>
2023-02-10 20:48:35 -0300

    activities of course database engineer by meta

---
+++ working_numbers.sql
@@ -0,0 +1,7 @@
+/* TASK1 */
+/* CREATE DATABASE cm_devices;
+USE cm_devices; 
+CREATE TABLE devices( deviceID int, deviceName varchar(50), price decimal); */
+
+/* TASK2 */
+/* CREATE TABLE stock(deviceID int, quantity int, totalCost decimal); */
