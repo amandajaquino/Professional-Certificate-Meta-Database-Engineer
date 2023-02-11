e54e36e02f740c27660b83fd1e50e38b2db1cac0
Amanda <jord_amanda@yahoo.com.br>
2023-02-10 21:25:41 -0300

    activities of course database engineer by meta

---
+++ working_strings.sql
@@ -0,0 +1,8 @@
+/* TASK1
+CREATE TABLE customers( username CHAR(9), fullName VARCHAR(100), email VARCHAR(255)); 
+Show tables;
+Show columns from customers;  */
+
+/* TASK2
+CREATE TABLE feedback(feedbackID CHAR(8), feedbackType VARCHAR(100), comment TEXT(500));
+SHOW columns from feedback; */
