9121c29b0866ec117004ab2c37751868afbb3547
Amanda <jord_amanda@yahoo.com.br>
2023-02-11 18:02:48 -0300

    activities from database engineer

---
+++ choosing_datatype.sql
@@ -0,0 +1,5 @@
+/* CREATE DATABASE cm_devices; */
+/* USE cm_devices; */
+/* CREATE TABLE invoice (customerName VARCHAR(50), orderDate DATE, quantity INT, prince DECIMAL); */
+/* show TABLES; */
+/* SHOW columns FROM invoice; */
---
+++ working_defaultvalues.sql
@@ -0,0 +1,10 @@
+/* CREATE DATABASE cm_devices; */
+/* USE cm_devices; */
+/* CREATE TABLE address(id int not null, street varchar(255), postcode varchar(10), town varchar(30) default "Harrow"); */
+
+/* show columns from address; */
+
+/*  DROP TABLE Address; */
+
+/* CREATE TABLE Address (id int NOT NULL,  street VARCHAR(255), postcode VARCHAR(10) DEFAULT "HA97DE", town VARCHAR(30) DEFAULT "Harrow"); */
+/* show columns from address;  */
